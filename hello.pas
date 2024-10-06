18d4c4a8fc51b44105e716d11b62e03f71abea2e
Kirill Sidorov <kir.sid06@gmail.com>
2024-10-06 17:50:44 +0300

    Добавление  языков русский английский французский

--- hello.pas
+++ hello.pas
@@ -1,1 +1,7 @@
 ﻿##
+var a := ReadString('Русский, English, Français');
+case a of
+  'Русский': Print('Привет друг');
+  'English': Print('Hello friend');
+  'Français': Print('Salut mon ami');
+end;
